c47c6a3407862675711fab2e854e1aa57b94633b
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2024-10-16 10:31:17 -0600

    Terminar actividad calculator.js
    se termino de manera correcta pasando todos los test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
 const add = function(a,b) {
-	return a + b
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num)=>{
+    return total + num
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, num)=>{
+    return total * num
+  },1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let fac = a
+  let total =a
+	for(let i= 1; i < b; i++){
+    
+    total *= fac
+  }
+  return total
+
+  //* se ouede usar mas facil math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a == 0){
+    return 1
+  } else {
+    let fac = 1
+    for(let i = 1; i <= a; i++){
+      fac *= i
+    }
+    return fac
+  }
 };
 
 // Do not edit below this line
