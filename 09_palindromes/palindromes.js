bad219cd1c0dd7162a77ddaf2d7db5bd8d489d98
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2024-10-16 11:03:52 -0600

    Terminar actividad 09_palindromes
    se termino la actividad de manera correcta, pasando todos los test de manera correcta

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+    const palindromes = function (a) {
+        let str = a.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()¿?¡]/g, "").toLowerCase();
+        str = str.replace(/\s+/g, "")
+    if(str == str.split("").reverse().join("")){
+        return true
+    } else {return false}
 };
 
 // Do not edit below this line
