6569b8330af3ccde63f275e2e3a41b5326a0739e
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2024-10-17 00:16:36 -0600

    Actividad fibonacci realizada, falta test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0){
+        return "OPPS"
+    } else if(n == 0 || n == 1){
+        return 1
+        } else{
+            for(let i = 0; i < n; i++){
+                n= (n-1) + (n-2)
+            }
 
+            return n
+        }
 };
 
 // Do not edit below this line
